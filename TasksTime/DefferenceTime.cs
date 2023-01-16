b5d8c6256071cb29366e0c517e43ae7278482527
Elvinp329 <elvinkj@code.edu.az>
2023-01-16 15:21:30 +0400

    TaskTime

---
+++ TasksTime/DefferenceTime.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksTime
+{
+    internal class DefferenceTime
+    {
+        internal string Defference(string time1, string time2, out string time3)
+        {
+
+            int h1 = int.Parse(time1.Substring(0, 2));
+            int m1 = int.Parse(time1.Substring(3, 2));
+            int s1 = int.Parse(time1.Substring(6, 2));
+
+            int h2 = int.Parse(time2.Substring(0, 2));
+            int m2 = int.Parse(time2.Substring(3, 2));
+            int s2 = int.Parse(time2.Substring(6, 2));
+
+            int h3, m3, s3;
+
+            s3 = s2 - s1;
+
+            if (s2 < s1)
+
+
+            {           
+                s3 = (s2 + 60) - s1;
+                m2 = m2 - 1; 
+            }
+
+            m3 = m2 - m1;
+
+            if (m2 < m1)
+            {
+                m3 = (m2 + 60) - m1;
+                h2 = h2 - 1; 
+            }
+
+            h3 = h2 - h1;
+
+            if (h2 < h1)
+
+            { h3 = (h2 + 24) - h1;
+            }
+
+            string x = h3.ToString();
+            string y = m3.ToString();
+            string z = s3.ToString();
+
+            if (h3 < 10 || m3 < 10 || s3 < 10)
+            {
+                x = h3.ToString().PadLeft(2, '0');
+                y = m3.ToString().PadLeft(2, '0');
+                z = s3.ToString().PadLeft(2, '0');
+            }
+
+
+
+            time3 = ($"{x}:{y}:{z}");
+
+            return time3;
+
+
+        }
+
+    }
+}
---
+++ TasksTime/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TasksTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var defferenceTime = new DefferenceTime();
+            Console.Write("Baslama vaxtini qeyd edin:");
+            string time1 = Console.ReadLine();
+            Console.Write("Bitme vaxtini qeyd edin:");
+            string time2 = Console.ReadLine();
+            string time3;
+            var result = defferenceTime.Defference(time1, time2, out time3);
+            Console.WriteLine(result.ToString());
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
